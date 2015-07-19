fe0135557a1748188f75d80b40dec72783008a5d
Clare Politano <clare@istrategylabs.com>
2015-07-19 18:49:39 -0400

    added gulp build and watch tasks

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,4 +1,1 @@
-var thisIsAnAmazingVar = 300;
-
-console.log('this js controls a nav ' + thisIsAnAmazingVar);
-console.log('this js controls a form');
+var thisIsAnAmazingVar=300;console.log("this js controls a nav "+thisIsAnAmazingVar),console.log("this js controls a form");
--- gulpfile.js
+++ gulpfile.js
@@ -7,26 +7,35 @@ var gulp = require('gulp'),
 
 
 // Creates a readable stream of data in memory
-gulp.task('concatScripts', function(){
-  gulp.src([
+gulp.task('concatScripts', function() {
+  return gulp.src([
     'src/assets/js/nav.js',
     'src/assets/js/form.js'])
   .pipe(concat('app.js')) // concats readable stream to one file
   .pipe(gulp.dest('dist/assets/js')); // persists file to destination
 });
 
-gulp.task('minifyScripts', function(){
+gulp.task('minifyScripts', ['concatScripts'], function() {
   gulp.src('dist/assets/js/app.js')
   .pipe(uglify())
   .pipe(gulp.dest('dist/assets/js'));
 });
 
-gulp.task('compileSass', function(){
+gulp.task('compileSass', function() {
   gulp.src('src/assets/sass/app.scss')
   .pipe(sass())
   .pipe(gulp.dest('dist/assets/css'));
 });
 
-gulp.task('default', ['concatScripts', 'minifyScripts', 'compileSass'], function(){
+gulp.task('watchSass', function() {
+  gulp.watch(['src/assets/sass/*.scss'], ['compileSass']);
+
+});
+
+gulp.task('build', ['minifyScripts', 'compileSass'], function() {
+  console.log('build task');
+});
+
+gulp.task('default', ['concatScripts', 'minifyScripts', 'compileSass'], function() {
   console.log('default task');
 });
